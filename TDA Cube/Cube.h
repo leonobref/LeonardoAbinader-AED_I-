f2f8bd02dde3c569f75c0629b941c4af72b373d4
leonobref <120611349+leonobref@users.noreply.github.com>
2023-05-21 22:49:13 -0400

    Delete TDA Cube directory

--- TDA Cube/Cube.h
+++
@@ -1,9 +0,0 @@
-typedef struct{
-    float side;
-} Cube;
-
-Cube *create_cube (float l);
-void destroy_cube (Cube *cub);
-float side_cube(Cube *cub);
-float area_cube(Cube *cub);
-float vol_cube(Cube *cub);
