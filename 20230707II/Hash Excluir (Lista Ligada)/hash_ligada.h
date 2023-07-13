1fc94351609e9f93e39967a38700c8567ee6cdca
leonobref <120611349+leonobref@users.noreply.github.com>
2023-07-13 18:13:54 -0400

    Update hash_ligada.h

--- 20230707II/Hash Excluir (Lista Ligada)/hash_ligada.h
+++ 20230707II/Hash Excluir (Lista Ligada)/hash_ligada.h
@@ -20,6 +20,8 @@ void inicializar_hash(Tabela_Hash);
 void inserir(Tabela_Hash, Paciente);
 void imprimir_hash(Tabela_Hash);
 Paciente* busca(Tabela_Hash , int);
+
+//Exercicio
 void excluir(Tabela_Hash, int);
 
 #endif
